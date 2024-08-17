0c6588ff73422617b95704d7983fbc29f2c7b159
anwam <dev.anuwong@gmail.com>
2024-08-17 15:02:30 +0700

    chore: note
    variables in rust are immutable by default
    to mutable the variables, provide mut after let to make variable mutable

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let x = 5;
+    let x = 5; // immutable variable
     println!("The value of x is: {x}");
-    x = 6;
+    x = 6; // error: cannot assign twice to immutable variable `x`
     println!("The value of x is: {x}");
 }
