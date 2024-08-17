e5afd6a5551c38635bbd8f97eb322d0367ab257d
anwam <dev.anuwong@gmail.com>
2024-08-17 14:52:45 +0700

    chore: cleanup

--- binary-search/target/debug/binary-search.d
+++
@@ -1,1 +0,0 @@
-/Users/anuwong/workspace/github.com/anwam/learn-rust-2024/binary-search/target/debug/binary-search: /Users/anuwong/workspace/github.com/anwam/learn-rust-2024/binary-search/src/main.rs
